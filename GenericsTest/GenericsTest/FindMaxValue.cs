3df5205b5d139ba5400b12fe017eb3c1a8684f48
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-22 14:58:32 +0530

    [Rajesh] Added : Generic Method Added

---
+++ GenericsTest/GenericsTest/FindMaxValue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTest
+{
+    internal class FindMaxValue
+    {
+        internal class MaxAmongThree
+        {
+            //Find maximum among three integers
+            public int MaxAmongThreeInt(int num1, int num2, int num3)
+            {
+                if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+                {
+                    return num1;
+                }
+                if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+                {
+                    return num2;
+                }
+                if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+                {
+                    return num3;
+                }
+                return num1;
+            }
+
+            //Find maximum among three floats
+            public float MaxAmongThreeFloat(float num1, float num2, float num3)
+            {
+                if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+                {
+                    return num1;
+                }
+                if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+                {
+                    return num2;
+                }
+                if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+                {
+                    return num3;
+                }
+                return num1;
+            }
+
+            //Find maximum among three strings
+            public string MaxAmongThreeString(string str1, string str2, string str3)
+            {
+                if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
+                {
+                    return str1;
+                }
+                if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0)
+                {
+                    return str2;
+                }
+                if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0)
+                {
+                    return str3;
+                }
+                return str1;
+            }
+
+
+
+        }
+    }
+}
--- GenericsTest/GenericsTest/Program.cs
+++ GenericsTest/GenericsTest/Program.cs
@@ -10,6 +10,7 @@
                 Console.WriteLine("\n1. Find maximum among Three Integers Values");
                 Console.WriteLine("2. Find maximum among Three Floats Values");
                 Console.WriteLine("2. Find maximum among Three String Values");
+                Console.WriteLine("4. Find maximum among Int, Float, and String using Generic Method");
 
                 Console.Write("Enter your choice : ");
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -51,6 +52,24 @@
 
                         //TC3
                         Console.WriteLine("Maximum string is : " + findMaxString.MaxNumberString("5", "7", "8"));
+                        break;
+                    case 4:
+                        GenericMethod genericMethod = new GenericMethod();
+
+                        //TC Integer
+                        Console.WriteLine("Maximum integer is : " + genericMethod.MaxAmongThree(8, 6, 5));
+                        Console.WriteLine("Maximum integer is : " + genericMethod.MaxAmongThree(6, 9, 7));
+                        Console.WriteLine("Maximum integer is : " + genericMethod.MaxAmongThree(5, 7, 8));
+
+                        //TC Float
+                        Console.WriteLine("Maximum float number is : " + genericMethod.MaxAmongThree(8.7f, 6.4f, 5.8f));
+                        Console.WriteLine("Maximum float number is : " + genericMethod.MaxAmongThree(6.4f, 9.1f, 7.3f));
+                        Console.WriteLine("Maximum float number is : " + genericMethod.MaxAmongThree(5.5f, 7.3f, 8.4f));
+
+                        //TC String 
+                        Console.WriteLine("Maximum string is : " + genericMethod.MaxAmongThree("8", "6", "5"));
+                        Console.WriteLine("Maximum string is : " + genericMethod.MaxAmongThree("6", "9", "7"));
+                        Console.WriteLine("Maximum string is : " + genericMethod.MaxAmongThree("5", "7", "8"));
                         break;
                     default:
                         Console.WriteLine("Please enter correct option from the list");
